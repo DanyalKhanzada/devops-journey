f30ba5b973c4edc4ef333df0dcc4f55a0b37411e
Danyal Khanzada <78877585+DanyalKhanzada@users.noreply.github.com>
2024-01-19 13:55:51 -0500

    custom route table

--- task#8/network.tf
+++ task#8/network.tf
@@ -1,6 +1,25 @@
+#Create Internet Gateway
+
 resource "aws_internet_gateway" "prod-igw" {
     vpc_id = "${aws_vpc.prod-vpc.id}"
     tags {
         Name = "prod-igw"
+    }
+}
+
+#  Creating Custom route table
+
+resource "aws_route_table" "prod-public-crt" {
+    vpc_id = "${aws_vpc.main-vpc.id}"
+    
+    route {
+        //associated subnet can reach everywhere
+        cidr_block = "0.0.0.0/0" 
+        //CRT uses this IGW to reach internet
+        gateway_id = "${aws_internet_gateway.prod-igw.id}" 
+    }
+    
+    tags {
+        Name = "prod-public-crt"
     }
 }
